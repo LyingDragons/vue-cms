938db16d7cd8305d51139f64b7713dda4b25f6fd
majie <1304908180@qq.com>
2020-01-15 15:31:32 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('ok')
+import Vue from 'vue'
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header);
+
+import './lib/mui/css/mui.css'
+import app from './App.vue'
+
+var vm=new Vue({
+	el:'#app',
+	render: c => c(app)
+})
